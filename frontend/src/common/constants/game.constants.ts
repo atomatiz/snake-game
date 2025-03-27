f3dcdef7d0f7d6eea92570d988448df93dd28664
atomatiz <thisismisterbrian@gmail.com>
2025-03-27 16:57:01 +0700

    chore: implemented api versioning

--- frontend/src/common/constants/game.constants.ts
+++ frontend/src/common/constants/game.constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  process.env.NEXT_PUBLIC_SNAKE_GAME_API_URL || "http://localhost:3001/api";
+  process.env.NEXT_PUBLIC_SNAKE_GAME_API_URL || "http://localhost:3001/v1/api";
