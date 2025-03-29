b1cb006f55e85e0a1841a86d03ff9fa6727d8643
atomatiz <thisismisterbrian@gmail.com>
2025-03-29 15:30:07 +0700

    hotfix: rendering replay state effectively

--- frontend/src/containers/GameContainer.tsx
+++ frontend/src/containers/GameContainer.tsx
@@ -5,7 +5,7 @@ import { GameBoard } from "@/components/game/GameBoard";
 import { GameForm } from "@/components/game/GameForm";
 import { GameInfo } from "@/components/game/GameInfo";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { startGameAsync, resetGame, newGame } from "@/store/slices/game.slice";
+import { startGameAsync, newGame } from "@/store/slices/game.slice";
 
 export const GameContainer: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -17,10 +17,9 @@ export const GameContainer: React.FC = () => {
     dispatch(startGameAsync({ width: w, height: h, moveInterval: interval }));
   };
 
-  const handleReplay = () => {
+  const handleReplay = async () => {
     if (width && height && moveInterval) {
-      dispatch(resetGame());
-      dispatch(startGameAsync({ width, height, moveInterval }));
+      await dispatch(startGameAsync({ width, height, moveInterval }));
     }
   };
 
